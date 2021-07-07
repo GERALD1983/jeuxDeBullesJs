5af171f5e02bd159942991a4c10f25188899f66a
ferron gerald <opstyle1@hotmail.fr>
2021-07-07 12:45:52 +0100

    jeux bulles js premier tir

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-let bulles = document.createElement("div");
+const colorArray = [
+  "black",
+  "blue",
+  "yellow",
+  "red",
+  "green",
+  "grey",
+  "aqua",
+  "brown",
+];
+counterText = document.querySelector("h3");
+counter = 1;
+const tirSon = new Audio("son/cartoon.mp3");
 
-bulles.classList.add("bulle");
-document.body.appendChild(bulles);
+const makeBubble = () => {
+  let random = colorArray[Math.floor(Math.random() * colorArray.length)];
+  let bulles = document.createElement("div");
 
-const size = Math.random() * 150 + 50 + "px";
+  bulles.style.backgroundColor = random;
 
-let bulle = document.querySelector(".bulle");
+  bulles.textContent = "TIR";
 
-bulle.style.height = size;
+  bulles.classList.add("bulle");
+  document.body.appendChild(bulles);
 
-bulle.style.width = size;
+  const size = Math.random() * 150 + 50 + "px";
+
+  bulles.style.height = size;
+
+  bulles.style.width = size;
+
+  const plusMoins = Math.random() < 0.5 ? 1 : -1;
+
+  bulles.style.top = Math.random() * 100 + 150 + "%";
+  bulles.style.left = plusMoins * 100 + "%";
+
+  bulles.style.setProperty("--left", Math.random() * 100 + "%");
+
+  bulles.addEventListener("click", () => {
+    tirSon.play();
+
+    bulles.remove();
+    counterText.textContent = counter++;
+  });
+};
+
+setInterval(makeBubble, 1000);
